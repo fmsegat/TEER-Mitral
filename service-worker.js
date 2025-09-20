e9e8ecb674b9e681653b973b65396b26270919f3
fmsegat <fmsegat@gmail.com>
2025-09-20 16:33:42 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+
+const CACHE_NAME = 'teer-mitral-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        // Opcional: cache dinâmico
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
